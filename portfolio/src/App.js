e76176b8ebbdff2e7d4df9fb0a79ffbb479d6d69
Kaustubh <kaustubhthakur76@gmail.com>
2023-01-16 01:27:00 +0530

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import useMediaQuery from "./hooks/useMediaQuery";
+function App() {
+  const [selectedPage,setSelectedPage] = useState('home')
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <div className="app bg-deep-blue">
+  <Navbar
+  selectedPage={selectedPage}
+  setSelectedPage={setSelectedPage}
+  />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import useMediaQuery from '../hooks/useMediaQuery'
+
+const Navbar = ({selectedPage,setSelectedPage}) => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
